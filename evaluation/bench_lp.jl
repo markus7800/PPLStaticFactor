328f3a72ade282001054862f16f8e61e4e1c2716
Markus Böck <markus.boeck7800@hotmail.com>
2025-03-14 11:40:58 +0100

    change N

--- evaluation/bench_lp.jl
+++ evaluation/bench_lp.jl
@@ -130,12 +130,12 @@ end
 
 # chosen such that every runtest takes 1-2s
 name_to_N = Dict{String,Int}(
-    "aircraft" => 75_000,
+    "aircraft" => 100_000,
     "captcha" => 1_000,
     "dirichlet_process" => 10_000,
     "geometric" => 500_000,
-    "gmm_fixed_numclust" => 25_000,
-    "gmm_variable_numclust" => 25_000,
+    "gmm_fixed_numclust" => 50_000,
+    "gmm_variable_numclust" => 50_000,
     "hmm_fixed_seqlen" => 100_000,
     "hmm_variable_seqlen" => 100_000,
     "hurricane" => 1_000_000,
@@ -146,7 +146,7 @@ name_to_N = Dict{String,Int}(
     "pedestrian" => 100_000,
     "urn" => 100_000,
 
-    "gmm_fixed_numclust_unrolled" => 25_000,
+    "gmm_fixed_numclust_unrolled" => 50_000,
     "hmm_fixed_seqlen_unrolled" => 100_000,
     "lda_fixed_numtopic_unrolled" => 10_000,
     "linear_regression_unrolled" => 100_000,
