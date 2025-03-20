9dd735d3e3cc03ee6e53ff63f34f5cc32e5ab5d7
Markus Böck <markus.boeck7800@hotmail.com>
2025-03-20 14:42:26 +0100

    paper results + print table

---
+++ print_table_1.py
@@ -0,0 +1,50 @@
+
+import pandas as pd
+
+df = pd.read_csv("evaluation/paper_lmh_results.csv", sep=",")
+
+row_start = {
+    "aircraft": "Aircraft & $\\infty$ & $1\\mathrm{e}{5}$",
+    "bayesian_network": "Bayesian Network & 37 & $1\\mathrm{e}{5}$",
+    "captcha": "Captcha & $\\infty$ & $1\\mathrm{e}{3}$",
+    "dirichlet_process": "Dirichlet process & $\\infty$ & $1\\mathrm{e}{4}$",
+    "geometric": "Geometric &  $\\infty$ & $5\\mathrm{e}{5}$",
+    "gmm_fixed_numclust": "GMM (fixed \\#clusters) & 209 & $5\\mathrm{e}{4}$",
+    "gmm_variable_numclust": "GMM (variable \\#clusters) & $\\infty$ & $5\\mathrm{e}{4}$",
+    "hmm": "HMM & 101 & $1\\mathrm{e}{5}$",
+    "hurricane": "Hurricane & 5 & $1\\mathrm{e}{6}$",
+    "lda_fixed_numtopic": "LDA (fixed \\#topics) & 551 & $1\\mathrm{e}{4}$",
+    "lda_variable_numtopic": "LDA (variable \\#topics) & $\\infty$ & $1\\mathrm{e}{4}$",
+    "linear_regression": "Linear regression & 102 & $1\\mathrm{e}{5}$",
+    "marsaglia": "Marsaglia & $\\infty$ & $5\\mathrm{e}{5}$",
+    "pedestrian": "Pedestrian & $\\infty$ & $1\\mathrm{e}{5}$",
+    "pcfg": "PCFG & $\\infty$ & $1\\mathrm{e}{5}$",
+    "urn": "Urn & $\\infty$ & $1\\mathrm{e}{5}$",
+    "hmm_unrolled": "HMM (unrolled loop) & 101 & $1\\mathrm{e}{5}$"
+}
+
+for _, row in df.iterrows():
+    ratio = row.rel_static
+    arrow = "$\\samearrow$"
+    color = "\\textcolor{Gray}"
+    if ratio < 0.8:
+        arrow = "$\\fastarrow$"
+        color = "\\textcolor{ForestGreen}"
+    if ratio < 0.5:
+        arrow = "$\\megafastarrow$"
+        color = "\\textcolor{ForestGreen}"
+    if ratio > 1.2:
+        arrow = "$\\slowarrow$"
+        color = "\\textcolor{OrangeRed}"
+    if ratio > 1.5:
+        arrow = "$\\megaslowarrow$"
+        color = "\\textcolor{OrangeRed}"
+
+    rel_str = color + "{" + f"{ratio:.2f}" + "} & " + color + "{" + arrow + "}"
+    print(row_start[row.model], "&", f"{row.acceptancerate:.2f}", "&", f"{row.none:.3f} & {row.static:.3f} & {rel_str}", end="")
+    if pd.isna(row.finite):
+        print(" & - & - \\\\")
+    else:
+        print(f" & {row.finite:.3f} & {row.finite/row.none:.2f} \\\\")
+
+
---
+++ print_table_2.py
@@ -0,0 +1,66 @@
+
+import pandas as pd
+
+df_ours = pd.read_csv("evaluation/paper_lmh_results.csv", sep=",")
+df_ours.set_index("model", inplace=True)
+
+df_gen = pd.read_csv("compare/gen/paper_results.csv", sep=",")
+df_gen.set_index("model", inplace=True)
+
+df_webppl = pd.read_csv("compare/webppl/paper_results.csv", sep=",")
+df_webppl.set_index("model", inplace=True)
+
+
+models = {
+    "aircraft": "Aircraft",
+    "bayesian_network": "Bayesian Network",
+    "captcha": "Captcha",
+    "dirichlet_process": "Dirichlet process",
+    "geometric": "Geometric",
+    "gmm_fixed_numclust": "GMM (fixed \\#clusters)",
+    "gmm_variable_numclust": "GMM (variable \\#clusters)",
+    "hmm": "HMM",
+    "hurricane": "Hurricane",
+    "lda_fixed_numtopic": "LDA (fixed \\#topics)",
+    "lda_variable_numtopic": "LDA (variable \\#topics)",
+    "linear_regression": "Linear regression",
+    "marsaglia": "Marsaglia",
+    "pedestrian": "Pedestrian",
+    "pcfg": "PCFG",
+    "urn": "Urn",
+}
+
+speed_up = True
+
+
+table_rows = []
+for model, model_name in models.items():
+    ours_rel = df_ours.loc[model].rel_static
+    gen_rel = df_gen.loc[model].rel
+    webppl_rel = df_webppl.loc[model].rel
+    # print(model_name, ours_rel, gen_rel, webppl_rel)
+
+    ours_str = f"{ours_rel:.2f}" if not speed_up else f"{1/ours_rel:.2f}"
+    if not (ours_rel > gen_rel or ours_rel > webppl_rel):
+        ours_str = "\\textbf{" + ours_str + "}"
+
+    if pd.isna(gen_rel):
+        gen_str = "-"
+    else:
+        gen_str = f"{gen_rel:.2f}" if not speed_up else f"{1/gen_rel:.2f}"
+        if not (gen_rel > webppl_rel or gen_rel > ours_rel):
+            gen_str = "\\textbf{" + gen_str + "}"
+
+    webppl_str = f"{webppl_rel:.2f}" if not speed_up else f"{1/webppl_rel:.2f}"
+    if not (webppl_rel > gen_rel or webppl_rel > ours_rel):
+        webppl_str = "\\textbf{" + webppl_str + "}"
+
+    table_rows.append(f"{model_name} & {ours_str} & {gen_str} & {webppl_str}")
+
+
+for i in range(len(table_rows) // 2):
+    print(table_rows[i], "&", table_rows[i + len(table_rows) // 2], "\\\\")
+
+    
+
+
